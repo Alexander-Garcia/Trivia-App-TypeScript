04d057cf65d93221e729889acadfbe147b0d6dd3
Alexander <adgbq2@mail.missouri.edu>
2022-01-30 13:45:55 -0600

    added lint and setup lint config

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import type { FormattedQuestion, Question } from '../types';
 /*
 * data: [
-* { 
+* {
 * category
 * correct_answer
 * difficulty
@@ -11,19 +11,15 @@ import type { FormattedQuestion, Question } from '../types';
 * ]
 */
 const shuffleArray = (answerArray: string[]) => [...answerArray].sort(() => Math.random() - 0.5);
-  
 
-
-export function formatData (data: Question[]): FormattedQuestion[] {
-  return data.map((d: Question): FormattedQuestion => {
-    return {
-      category: d.category,
-      correctAnswer: d.correct_answer,
-      difficulty: d.difficulty,
-      incorrectAnswers: d.incorrect_answers,
-      question: d.question,
-      type: d.type,
-      answers: shuffleArray([ ...d.incorrect_answers, d.correct_answer ]),
-    };
-  });
+export function formatData(data: Question[]): FormattedQuestion[] {
+  return data.map((d: Question): FormattedQuestion => ({
+    category: d.category,
+    correctAnswer: d.correct_answer,
+    difficulty: d.difficulty,
+    incorrectAnswers: d.incorrect_answers,
+    question: d.question,
+    type: d.type,
+    answers: shuffleArray([...d.incorrect_answers, d.correct_answer]),
+  }));
 }

c729144df9d7a1ca18c8a1539498a3f7a59d3d04
Alexander <adgbq2@mail.missouri.edu>
2022-01-29 20:49:57 -0600

    set up game config component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { useState } from 'react';
+import GameConfig from './components/GameConfig';
+
 function App() {
+  const [nmbrOfQuestions, setNmbrOfQuestions] = useState('');
+  const [selectOption, setSelectOption] = useState('Easy');
+
+  const handleSubmit = () => {
+   console.log('nmbrOfquestions', nmbrOfQuestions); 
+   console.log('select Option', selectOption); 
+  }
+
   return (
-   <div>Hello world</div> 
+    <div className="App">
+      <h1>Trivia</h1>
+      <GameConfig 
+        handleSubmit={handleSubmit}
+        nmbrOfQuestions={nmbrOfQuestions}
+        setNmbrOfQuestions={setNmbrOfQuestions}
+        selectOption={selectOption}
+        setSelectOption={setSelectOption}
+      />
+   </div>
   );
 }
 

12bff03fcd7deb5c110ad786c7b3a52689650de4
Alexander <adgbq2@mail.missouri.edu>
2022-01-29 20:52:23 -0600

    npm axios updates to props and react types

---
+++ src/components/GameConfig.tsx
@@ -0,0 +1,50 @@
+import { useCallback } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+/*
+* The options that trivia api can take are 
+* Number of questions
+* difficulty: string; easy, medium, difficult
+*/
+type Props = {
+  handleSubmit: () => void;
+  nmbrOfQuestions: string;
+  setNmbrOfQuestions: Dispatch<SetStateAction<string>>;
+  selectOption: string;
+  setSelectOption: Dispatch<SetStateAction<string>>;
+};
+
+function GameConfig ({
+  handleSubmit,
+  nmbrOfQuestions,
+  setNmbrOfQuestions,
+  setSelectOption,
+}: Props) {
+  const selectOptions = ['Easy', 'Medium', 'Difficult'];
+
+  const handleSelectChange = useCallback((e) => {
+    setSelectOption(e.target.value);
+  }, []);
+
+  const handleInputChange = useCallback((e) => {
+    setNmbrOfQuestions(e.target.value);
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="nmbrOfQuestions">Total Number of Questions:</label>
+        <input onChange={handleInputChange} value={nmbrOfQuestions} type="text" placeholder="Set Number of Questions" id="nmbrOfQuestions" required/>
+      </div>
+      <div>
+        <label htmlFor="difficulty">Choose Difficulty:</label>
+        <select id="difficulty" onChange={handleSelectChange}>
+          {selectOptions.map((option) => <option key={option} value={option}>{option}</option>)}
+        </select>
+      </div>
+      <button onClick={handleSubmit}>Begin Game</button>
+    </div>
+  )
+}
+
+export default GameConfig;
+
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: string;
+  questionNumber: number;
+  totalQuestions: number;
+}
+
+function QuestionCard () {
+  return (
+  <div>
+      questionCard
+  </div>
+);
+}
+
+export default QuestionCard;
